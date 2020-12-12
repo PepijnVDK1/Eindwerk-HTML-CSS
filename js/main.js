ccb0ba20b2a5bd41710525e24e2d4080775e6289
Pepijn <pepijn_vdk@hotmail.com>
2020-12-12 21:45:39 +0100

    Minor tweaks

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ let nav = document.getElementById("header");
 let subnav = document.getElementById("nav");
 
 window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 200) {
-    nav.style.backgroundColor = "rgb(22 24 23 / 70%)";
-  } else if (window.pageYOffset < 200) {
-    nav.style.backgroundColor = "transparent";
+  if (window.innerWidth > 800) {
+    if (window.pageYOffset > 200) {
+      nav.style.backgroundColor = "rgb(22 24 23 / 70%)";
+    } else if (window.pageYOffset < 200) {
+      nav.style.backgroundColor = "transparent";
+    }
   }
 });
 
